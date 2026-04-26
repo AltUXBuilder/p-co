84f618d776d5e970a67116a0343ce06a39312e70
AltUXBuilder <aurorafxc@gmail.com>
2026-04-26 22:29:18 +0100

    Delete pco_v2/pages directory

--- pco_v2/pages/auth/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../../includes/bootstrap.php';
-if (is_logged_in()) audit_log('user_logout','user',current_user_id());
-session_kill();
-redirect('/pages/auth/login.php');
